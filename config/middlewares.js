25418a272a54b74a17c4f5bd4e5a15db40882c91
Anh Dinh <a.dinh26101@gmail.com>
2023-12-12 14:15:16 +0700

    strapi blog

---
+++ config/middlewares.js
@@ -0,0 +1,29 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            ("https://fj-blog.s3.ap-southeast-1.amazonaws.com/"),
+          ],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
+];
